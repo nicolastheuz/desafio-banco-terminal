4b7401b752d4d3f005034bfc970c6a4294c4cf4e
nicolastheuz <nicolasmatheus0101@outlook.com>
2025-07-21 22:15:32 -0300

    meu projeto conta banco v.2

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,17 @@
+import java.util.Scanner; //Importação do scanner
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+       //Colocar o Scanner no código
+       var Scanner = new Scanner(System.in);
+       System.out.println("Informe sua agência ");
+       var agencia = Scanner.next();
+       System.out.println("Digite o Número da conta ");
+       var conta = Scanner.next();
+       System.out.println("Digite o seu nome ");
+       var nome_cliente = Scanner.next();
+        double saldo = 12.500;
+        System.out.printf("Olá %s, obrigado por criar uma conta em nosso banco, sua agência é %s, conta %s e seu saldo %.3f já está disponível para saque", nome_cliente, agencia, conta, saldo);
+
+
     }
 }
